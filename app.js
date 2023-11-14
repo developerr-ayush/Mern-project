ebd5bee5a588ef321cd15fb950000c532be61fc7
shahayush480 <shahayush480@gmail.com>
2023-11-14 09:06:16 +0530

    day 2 complete

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 const PORT = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/listing";
 const { Listing } = require("./models/listing.js");
-const path = require("path")
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 main()
   .then(() => {
     console.log("connected to DB");
@@ -17,8 +22,22 @@ async function main() {
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
 });
+// show all listings
 app.get("/", async (req, res) => {
   let allListings = await Listing.find({});
-  console.log(alllistings)
-  res.render("listings/home", allListings);
+  res.render("home", { allListings });
+});
+app.get("listing/new", (req,res)=>{
+  res.render("listing/add")
+})
+app.post("listing/new", (req,res)=>{
+  // res.render("listing/add")
+})
+
+// show details
+app.get("/listing/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  let dataList = await Listing.findById(id);
+  res.render("listing/details", { dataList });
 });
